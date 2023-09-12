4d6d1b5e457a8ee4ab68b9ec66d7002080e8fe5d
super-nived <nivedchandran7@gmail.com>
2023-09-12 14:10:28 +0530

    Updated settings (adding static_root )

--- Timebank/settings.py
+++ Timebank/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Directory on the filesystem to collect static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
