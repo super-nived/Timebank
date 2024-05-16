ad99ad33582114c3582b59077b1aba7ebc98b74a
nived chandran <nived-c@industryapps.net>
2024-05-16 12:42:18 +0530

    updated settings.p

--- Timebank/settings.py
+++ Timebank/settings.py
@@ -121,14 +121,16 @@ USE_TZ = True
 # for local running
 
 # STATIC_URL = 'static/'
-# # Directory on the filesystem to collect static files.
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# MEDIA_URL='/media/'
-# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 
 
 # for production 
 STATIC_URL = '/static/'
+
+# Directory on the filesystem to collect static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
